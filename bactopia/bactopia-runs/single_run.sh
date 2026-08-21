a197a10fdbe3d1ea338b82de4362bd8b510e9c54
umar1234500 <dr.umarbinfarooq@gmail.com>
2026-08-21 16:16:31 +0800

    add_single_run.sh

---
+++ bactopia/bactopia-runs/single_run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#  single run script for Bactopia
+bactopia \
+    --accession SRX4563634 \
+    --coverage 100 \
+    --genome_size 2800000 \
+    --max_cpus 2 \
+    --outdir ena-single-sample
+    
